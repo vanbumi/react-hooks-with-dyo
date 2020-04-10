5d4a148a4e32ce4538fd1b240dc3efd644c7f3de
vanbumi <jsp.dio@gmail.com>
2020-04-10 20:17:08 +0700

    Create toggle on of with useState

--- src/Counter.js
+++ src/Counter.js
@@ -3,16 +3,46 @@ import React, { useState } from 'react';
 const Counter = () => {
 
     const [ count, setCount ] = useState(0);
+    const [ isOn, setIsOn ] = useState(false);
 
     const incrementHandle = () => {
         setCount(prevCount => prevCount + 1)
     } 
 
+    const toggleLigth = () => {
+        setIsOn(prevIsOn => !prevIsOn)
+    }
+
     return (
-        <div>
-            <h3>Button Count has click { count } times</h3>
-            <button onClick={incrementHandle}>Click Me</button>
-        </div>
+            <div>
+                <h2>Counter</h2>
+                <h3>Button Count has click { count } times</h3>
+                <button onClick={incrementHandle}>Click Me</button>
+         
+                <h2>Toggle Ligth</h2>
+                {/* <div
+                    style={{
+                        height: '50px',
+                        width: '50px',
+                        backgroundColor: isOn ? 'yellow' : 'grey'
+                    }}
+                    onClick={toggleLigth}
+                >
+                </div> */}
+                <img 
+                    src={ isOn 
+                        ? "https://icon.now.sh/highlight/fd0" 
+                        : "https://icon.now.sh/highlight/aaa"
+                    }
+                    style={{
+                        height: '50px',
+                        width: '50px',
+                    }}  
+                    alt='flash-light'
+                    onClick={toggleLigth}          
+                />           
+            </div>
+    
     )
 }
 
